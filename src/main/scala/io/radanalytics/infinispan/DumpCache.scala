b720378a825cb1db7f0632606a1bd28c5536f6df
eldritchjs <jschlessman@gmail.com>
2018-04-02 12:27:28 -0400

    Added files

---
+++ src/main/scala/io/radanalytics/infinispan/DumpCache.scala
@@ -0,0 +1,46 @@
+package io.radanalytics.infinispan
+
+import java.lang.Long
+
+import scala.util.Random
+
+import org.infinispan._
+import org.infinispan.client.hotrod.RemoteCache
+import org.infinispan.client.hotrod.RemoteCacheManager
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder
+import org.infinispan.client.hotrod.configuration.Configuration
+import org.infinispan.client.hotrod.impl.ConfigurationProperties
+
+import scala.collection.immutable
+import scala.util.Properties
+
+object DumpCache {
+
+  def getProp(camelCaseName: String, defaultValue: String): String = {
+        val snakeCaseName = camelCaseName.replaceAll("(.)(\\p{Upper})", "$1_$2").toUpperCase()
+            Properties.envOrElse(snakeCaseName, Properties.scalaPropOrElse(snakeCaseName, defaultValue))
+              }  
+
+  def main(args: Array[String]): Unit = {
+    val infinispanHost = getProp("jdgHost", "datagrid-hotrod")
+    val infinispanPort = getProp("jdgPort", "11222").toInt
+    val iterations = getProp("ccIter", "68").toInt
+    
+    val builder: ConfigurationBuilder = new ConfigurationBuilder()
+    builder.addServer().host(infinispanHost).port(infinispanPort)
+    val cacheManager = new RemoteCacheManager(builder.build())
+
+    var cache = cacheManager.getCache[String, String]()
+    var i: Int = 0 
+    var ret: String = "" 
+    for (i <- 1 to iterations) {
+      for (k <- cache.keySet.toArray) {
+        ret = cache.get(k)
+        println(k + ": " + ret)
+      }
+      Thread.sleep(10000)
+    }
+    cacheManager.stop()
+  }
+ 
+}
